898b8eb5f055a210f90228bb98cf8ebb423ccbd1
Mathieu Lecarme <mathieu@garambrogne.net>
2011-04-27 22:34:21 +0200

    first shoot

---
+++ src/main/java/org/garambrogne/ucengine/App.java
@@ -0,0 +1,13 @@
+package org.garambrogne.ucengine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/garambrogne/ucengine/WebClient.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package org.garambrogne.ucengine;
+
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+import org.jboss.netty.handler.codec.http.DefaultHttpRequest;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jboss.netty.handler.codec.http.HttpMethod;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpVersion;
+
+/**
+ * @author mlecarme
+ * 
+ */
+public class WebClient {
+	private boolean ssl = false;
+
+	public WebClient(URI uri) {
+		String scheme = uri.getScheme() == null ? "http" : uri.getScheme();
+		String host = uri.getHost() == null ? "localhost" : uri.getHost();
+		int port = uri.getPort();
+		if (port == -1) {
+			if (scheme.equalsIgnoreCase("http")) {
+				port = 80;
+			} else if (scheme.equalsIgnoreCase("https")) {
+				port = 443;
+			}
+		}
+		// Configure the client.
+		ClientBootstrap bootstrap = new ClientBootstrap(
+				new NioClientSocketChannelFactory(
+						Executors.newCachedThreadPool(),
+						Executors.newCachedThreadPool()));
+		// Set up the event pipeline factory.
+		bootstrap.setPipelineFactory(new HttpClientPipelineFactory(ssl));
+
+		// Start the connection attempt.
+		ChannelFuture future = bootstrap.connect(new InetSocketAddress(host,
+				port));
+
+		// Wait until the connection attempt succeeds or fails.
+		Channel channel = future.awaitUninterruptibly().getChannel();
+		if (!future.isSuccess()) {
+			future.getCause().printStackTrace();
+			bootstrap.releaseExternalResources();
+			return;
+		}
+
+		// Prepare the HTTP request.
+		HttpRequest request = new DefaultHttpRequest(HttpVersion.HTTP_1_1,
+				HttpMethod.GET, uri.toASCIIString());
+		request.setHeader(HttpHeaders.Names.HOST, host);
+		request.setHeader(HttpHeaders.Names.CONNECTION,
+				HttpHeaders.Values.CLOSE);
+		request.setHeader(HttpHeaders.Names.ACCEPT_ENCODING,
+				HttpHeaders.Values.GZIP);
+		// Send the HTTP request.
+		channel.write(request);
+
+		// Wait for the server to close the connection.
+		channel.getCloseFuture().awaitUninterruptibly();
+
+		// Shut down executor threads to exit.
+		bootstrap.releaseExternalResources();
+	}
+	
+	public void get(String url) {
+		
+	}
+
+}
