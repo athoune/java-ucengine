e84a2a474d0180fc30f465f4c9532d93bdd48cc3
Mathieu Lecarme <mathieu@garambrogne.net>
2011-05-17 23:19:35 +0200

    exceptions

---
+++ ucengine-core/src/main/java/org/garambrogne/ucengine/HttpException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.garambrogne.ucengine;
+
+/**
+ * @author mlecarme
+ *
+ */
+public class HttpException extends Exception {
+	private static final long serialVersionUID = 5092428484233787207L;
+
+	public HttpException(Throwable cause) {
+		super(cause);
+	}
+
+}
---
+++ ucengine-core/src/main/java/org/garambrogne/ucengine/UceException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.garambrogne.ucengine;
+
+/**
+ * @author mlecarme
+ *
+ */
+public class UceException extends Exception {
+	private static final long serialVersionUID = -2132280917125236628L;
+
+	public UceException() {
+		super();
+	}
+
+	public UceException(String message) {
+		super(message);
+	}
+
+}
